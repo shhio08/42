33d42f10aab663d0ced2f5d65196facc4f5b2dce
shhio08 <kiiro18.sh@gmail.com>
2022-10-28 00:22:27 +0900

    20221027

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: stakimot <stakimot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 13:35:31 by stakimot          #+#    #+#             */
-/*   Updated: 2022/10/23 13:55:23 by stakimot         ###   ########.fr       */
+/*   Updated: 2022/10/28 00:20:13 by stakimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void	*ft_memset(void *buf, int ch, size_t n)
 {
 	size_t i = 0;
-	char *str;
+	unsigned char *str;
 
-	str = (char *)buf;
+	str = (unsigned char *)buf;
 	while (i < n)
 	{
 		str[i] = (unsigned char)ch;

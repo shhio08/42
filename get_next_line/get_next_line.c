7f59a6e2436eabc256ef048b167676d8711a2915
stakimot <kiiro18.sh@gmail.com>
2022-11-30 13:16:49 +0900

    20221130

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: stakimot <stakimot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 13:24:49 by stakimot          #+#    #+#             */
-/*   Updated: 2022/11/27 15:37:01 by stakimot         ###   ########.fr       */
+/*   Updated: 2022/11/30 13:14:44 by stakimot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,19 +26,20 @@ char *read_buff(int fd)
 		return (NULL);
 	while (!ft_strchr(save, '\n'))
 	{
-		byte = read(fd, buff, 4);
-		if (byte == -1)
-			return (NULL);
-		// save = (char *)malloc(sizeof(char) * (ft_strlen(tmp) + BUFFER_SIZE + 1));
-		// if (!save)
-		// 	return (NULL);
+		byte = read(fd, buff, BUFFER_SIZE);
+		if (byte <= 0)
+			break;
+		buff[BUFFER_SIZE] = '\0';
 		save = ft_strjoin(tmp, buff);
 		free(tmp);
-		free(buff);
-		// tmp = (char *)malloc(sizeof(char) * (ft_strlen(save) + 1));
-		tmp = save;
+		tmp = ft_strdup(save);
 	}
+	free(buff);
+	buff = NULL;
 	free(tmp);
+	tmp = NULL;
+	if (byte == -1)
+		return (NULL);
 	return (save);
 }
 
@@ -54,7 +55,7 @@ char *make_line(char *buff, char *save)
 	cnt = 0;
 	b_cnt = 0;
 	s_len = ft_strlen(save);
-	while (buff[len] != '\n')
+	while (buff[len] != '\n' && buff[len] != '\0')
 		len++;
 	line = (char *)malloc(sizeof(char) * (s_len + len + 2));
 	if (!line)
@@ -66,10 +67,12 @@ char *make_line(char *buff, char *save)
 	}
 	while (cnt < s_len + len)
 		line[cnt++] = buff[b_cnt++];
-	line[cnt++] = '\n';
+	if (buff[b_cnt] == '\n')
+		line[cnt++] = '\n';
 	line[cnt] = '\0';
 	return (line);
 }
+
 char *make_save(char *buff)
 {
 	size_t	len;
@@ -79,9 +82,10 @@ char *make_save(char *buff)
 
 	b_cnt = 0;
 	s_cnt = 0;
-	while (buff[b_cnt] != '\n')
+	while (buff[b_cnt] != '\n' && buff[b_cnt] != '\0')
 		b_cnt++;
-	b_cnt += 1;
+	if (buff[b_cnt] == '\n')
+		b_cnt++;
 	len = ft_strlen(buff) - b_cnt;
 	save = (char *)malloc(sizeof(char) * len + 1);
 	if (!save)
@@ -97,39 +101,40 @@ char	*get_next_line(int fd)
 	char		*buff;
 	static char	*save;
 	char		*line;
-	// int			cnt;
 
-	
 	buff = read_buff(fd);
 	if (!buff)
 		return (NULL);
 	line = make_line(buff, save);
+	printf("a:%s", line);
 	if (!line)
 		return (NULL);
 	save = make_save(buff);
 	if (!save)
 		return (NULL);
+	free(buff);
+	buff = NULL;
 	return (line);
 }
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdio.h>
+#include <stdlib.h>
+
 int	main()
 {
 	int fd;
 	char *gnl;
+	int i=0;
 
 	fd = open("test.txt", O_RDONLY);
-	// while (1)
-	// {
-		// gnl = get_next_line(fd);
-		// if (!gnl)
-		// 	return (0);
-		// printf("%s", gnl);
-	// }
-	gnl = get_next_line(fd);
-	printf("%s", gnl);
+	while (1)
+	{
+		gnl = get_next_line(fd);
+		printf("%d:%s",i++ , gnl);
+		if (!gnl)
+			return (0);
+	}
+	// gnl = get_next_line(fd);
+	// printf("%s", gnl);
+	system("leaks a.out");
 	return(0);
 }
